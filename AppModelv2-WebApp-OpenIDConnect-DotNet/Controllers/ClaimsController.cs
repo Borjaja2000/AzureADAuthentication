4e0a49e529f400820e8ae52e5da7c35644fc4265
Borja Jaudenes <borjajaudenes@gmail.com>
2021-04-23 23:12:35 +0200

    Add project files.

---
+++ AppModelv2-WebApp-OpenIDConnect-DotNet/Controllers/ClaimsController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace AppModelv2_WebApp_OpenIDConnect_DotNet.Controllers
+{
+    [Authorize]
+    public class ClaimsController : Controller
+    {
+        public ActionResult Index()
+        {
+            var userClaims = User.Identity as System.Security.Claims.ClaimsIdentity;
+            ViewBag.Name = userClaims?.FindFirst("name")?.Value;
+            ViewBag.Username = userClaims?.FindFirst("preferred_username")?.Value;
+            ViewBag.Subject = userClaims?.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier)?.Value;
+            ViewBag.TenantId = userClaims?.FindFirst("http://schemas.microsoft.com/identity/claims/tenantid")?.Value;
+
+            return View();
+        }
+    }
+}
